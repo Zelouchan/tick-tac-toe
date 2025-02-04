1a4665a7b27247c7c2b37329f29b914548332936
Zelouchan <bad1dje@gmail.com>
2025-02-04 15:18:06 +0000

    Started game play logic

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
-
 const createPlayer = (name) => {
-    const player = () => 'player';
-    return {name, player};
+  const player = () => "player";
+  return { name, player };
 };
 
-const player1 = createPlayer('Tamara'); //here take input from UI for name
-const player2 = createPlayer('Andrew'); // here also
+const player1 = createPlayer("Tamara"); //here take input from UI for name
+const player2 = createPlayer("Ruth"); // here also
 
 const gameFlow = {
-    players: [player1, player2],
-    currentTurn: 0, 
+  players: [player1, player2],
+  currentTurn: 0,
+  board: Array(9).fill(""), //creates the empty board
 
-    nextTurn: function () {
-        const currentPlayer = this.currentTurn % 2 === 0 ? this.players[0] : this.players[1];
-        this.currentTurn++;
-        return currentPlayer;
-    }};
-    
-  //user turns
-  // function check winner
-  // 9 turns no winner calls tie
-  // game flow if when loop 0,2,4,6,8 player1.symbol or 1,3,5,7,9 player2.symbol
+  nextTurn: function () {
+    return this.players[this.currentTurn % 2];
+  },
+};
 
 
 function gameBoard() {
-  const emptyBoard = Array(9).fill('');
-  // create var selected
-  // if selected !== '' alert.pick empty
-  return emptyBoard;
-  //funtion to check if array position is emply give alert otherwise
-}
+  while (gameFlow.currentTurn <9) {
+  let input = parseInt(prompt(gameFlow.nextTurn().name + " Which Square do you want to claim? 0-8"));
 
-console.log(gameFlow);
-console.log(player1.name);
-console.log(player2.name);
-console.table(gameBoard());
+  if (isNaN(input) || input < 0 || input > 8) {
+    alert("Pick a number between 0 and 8");
+    return gameBoard();
+  }
+
+  if (gameFlow.board[input] !== "") {
+    alert("Pick An Empty Square");
+    continue;
+  } else {
+    const value = gameFlow.nextTurn().name; 
+    gameFlow.board[input] = value; 
+    gameFlow.currentTurn++; 
+  }
+
+  console.table(gameFlow.board); // Fixed: prints board after every move
+}}
+
+const winningCondition = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+gameBoard();
+
