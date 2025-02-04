a31d759dd4d12a996608e4cd75ec4546928788cb
Zelouchan <bad1dje@gmail.com>
2025-02-04 20:33:31 +0000

    added buttons, form input for names

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const createPlayer = (name, symbol) => {
-  return { name, symbol};
+  return { name, symbol };
 };
 
-const player1 = createPlayer(prompt("Who is Player 1?"), "X");
-const player2 = createPlayer(prompt("Who is Player 2?"), "O");
-const area = document.getElementById("game");
+let player1;
+let player2;
 
 const gameFlow = {
   players: [player1, player2],
@@ -16,6 +15,7 @@ const gameFlow = {
   },
 
   displayBoard: function () {
+    const area = document.getElementById("game"); // Get area here
     area.innerHTML = "";
     this.board.forEach((value, index) => {
       const newDiv = document.createElement("button");
@@ -23,6 +23,17 @@ const gameFlow = {
       newDiv.dataset.index = index;
       newDiv.addEventListener("click", move);
       area.appendChild(newDiv);
+    });
+  },
+
+  resetButton: function () {
+    const resets = document.getElementById("reset");
+    resets.addEventListener("click", () => {
+      const area = document.getElementById("game"); // Get area here
+      area.innerHTML = "";
+      this.board = Array(9).fill("");
+      this.currentTurn = 0;
+      this.displayBoard();
     });
   },
 };
@@ -38,10 +49,10 @@ function checkWinner() {
 
     if (
       gameFlow.board[a] &&
-      gameFlow.board[a] === gameFlow.board[b] && 
-      gameFlow.board[a] === gameFlow.board[c] ) {
-        
-      const winningSymbol = gameFlow.board[a]; 
+      gameFlow.board[a] === gameFlow.board[b] &&
+      gameFlow.board[a] === gameFlow.board[c]
+    ) {
+      const winningSymbol = gameFlow.board[a];
       const winner = gameFlow.players.find(player => player.symbol === winningSymbol);
       return winner.name;
     }
@@ -69,8 +80,23 @@ function move(event) {
     }
   }
 }
-function gameBoard() {
-  gameFlow.displayBoard();
+
+function submitButton() {
+  const starts = document.getElementById("submit");
+  starts.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const play1 = document.getElementById("name1").value;
+    const play2 = document.getElementById("name2").value;
+
+    player1 = createPlayer(play1, "X");
+    player2 = createPlayer(play2, "O");
+
+    gameFlow.players = [player1, player2]; // Assign players
+
+    gameFlow.displayBoard();
+  });
 }
 
-gameBoard();
+gameFlow.resetButton();
+submitButton();
